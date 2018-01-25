7d6fdaf2bbecd9674801babe46c54fa3dfb1b4ff
HWS <15757135983@163.com>
2018-01-25 16:19:12 +0800

    1.在webpack.base.conf配置babel-polyfill,解决ie浏览器不兼容promise用法问题

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,7 +13,8 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
+    "babel-polyfill":"babel-polyfill",//用来解决的兼容性
   },
   output: {
     path: config.build.assetsRoot,
